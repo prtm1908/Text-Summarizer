a917b511de372b4be84d09b0f90e31d6ee94c9b9
prtm1908 <prathambatra19@gmail.com>
2023-09-26 01:06:38 +0530

    .

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md','r',encoding='utf-8') as f:
 
 __version__="0.0.0"
 
-REPO_NAME="Text-Summarizer"
+REPO_NAME="Text-Summarizer-Transformers"
 AUTHOR_USER_NAME='prtm1908'
 SRC_REPO='textSummarizer'
 AUTHOR_EMAIL='prathambatra19@gmail.com'
